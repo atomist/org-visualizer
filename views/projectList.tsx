0c2edf39dcb7f39723a727ddd0d6e71b98a8e292
Jessica Kerr <jessitron@atomist.com>
2019-06-02 17:10:42 -0500

    List org above projects

--- views/projectList.tsx
+++ views/projectList.tsx
@@ -1,4 +1,5 @@
 import { RepoRef } from "@atomist/automation-client";
+import * as _ from "lodash";
 import * as React from "react";
 
 export type ProjectForDisplay = RepoRef; // will likely add more data later
@@ -19,11 +20,19 @@ function toListItem(p: ProjectForDisplay): React.ReactElement {
     </li >;
 }
 
+function displayOrgProjects(owner: string, projects: ProjectForDisplay[]): React.ReactElement {
+    return <div>
+        <h3>{owner}</h3>
+        <ul>
+            {projects.map(toListItem)}
+        </ul>
+    </div>;
+}
+
 export function ProjectList(props: ProjectListProps): React.ReactElement {
+    const projectsByOrg = _.groupBy(props.projects, p => p.owner);
     return <div>
         <h2>{props.projects.length} projects</h2>
-        <ul>
-            {props.projects.map(toListItem)}
-        </ul>
+        {Object.entries(projectsByOrg).map(kv => displayOrgProjects(...kv))}
     </div>;
 }
