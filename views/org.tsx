3b75f5e78a9b4e48029c5b408b7bd3556e275121
Jessica Kerr <jessitron@atomist.com>
2019-07-05 12:51:17 -0500

    Do not expand empty ones

--- views/org.tsx
+++ views/org.tsx
@@ -64,7 +64,7 @@ function idealDisplay(af: MaybeAnIdeal): React.ReactElement {
 function displayImportantFeature(f: FeatureForDisplay, i: number): React.ReactElement {
 
     const key = "collapsible" + i;
-    const expandByDefault = f.fingerprints.length <= 1;
+    const expandByDefault = f.fingerprints.length === 1;
 
     return <div className="wrap-collabsible feature-collapsible">
         <input id={key} className="sneaky toggle" type="checkbox" defaultChecked={expandByDefault}></input>
