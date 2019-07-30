343a27f12af569ce93e7e25375e3623948f164fe
Christian Dupuis <cd@atomist.com>
2019-07-30 11:18:15 +0200

    Add SdmDeps aspect

--- index.ts
+++ index.ts
@@ -81,7 +81,7 @@ const mode = process.env.ATOMIST_ORG_VISUALIZER_MODE || "online";
 export const configuration: Configuration = configure(async sdm => {
         const isStaging = sdm.configuration.endpoints.api.includes("staging");
 
-        const optionalAspects = isStaging ? [LeinDeps] : [];
+        const optionalAspects = isStaging ? [SdmDeps, LeinDeps] : [];
 
         const jobAspects = [
             DockerFrom,
