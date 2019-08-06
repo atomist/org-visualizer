d3352efa94e0fe75ff8329cd4f41786a6f8474ab
Jim Clark <slimslenderslacks@gmail.com>
2019-08-06 16:18:19 -0700

    LeinDeps were broken because of the missing registerReportDetails

--- index.ts
+++ index.ts
@@ -86,101 +86,108 @@ import { orgPage } from "./lib/routes/orgPage";
 const mode = process.env.ATOMIST_ORG_VISUALIZER_MODE || "online";
 
 export const configuration: Configuration = configure(async sdm => {
-        const isStaging = sdm.configuration.endpoints.api.includes("staging");
+    const isStaging = sdm.configuration.endpoints.api.includes("staging");
 
-        const optionalAspects = isStaging ? [LeinDeps] : [];
+    const optionalAspects = isStaging ? [LeinDeps] : [];
 
-        const jobAspects = [
-            DockerFrom,
-            DockerfilePath,
-            DockerPorts,
-            SpringBootStarter,
-            TypeScriptVersion,
-            NpmDeps,
-            TravisScriptsAspect,
-            StackAspect,
-            CiAspect,
-            JavaBuild,
-            SpringBootVersion,
-            DirectMavenDependencies,
-            ...optionalAspects,
-        ];
-        const handlers = [];
+    const jobAspects = [
+        DockerFrom,
+        DockerfilePath,
+        DockerPorts,
+        SpringBootStarter,
+        TypeScriptVersion,
+        NpmDeps,
+        TravisScriptsAspect,
+        StackAspect,
+        CiAspect,
+        JavaBuild,
+        SpringBootVersion,
+        DirectMavenDependencies,
+        ...optionalAspects,
+    ];
+    const handlers = [];
 
-        // TODO cd merge into one call
-        registerCategories(TypeScriptVersion, "Node.js");
-        registerReportDetails(TypeScriptVersion, {
-            name: "TypeScript versions",
-            shortName: "version",
-            unit: "version",
-            url: "fingerprint/typescript-version/typescript-version?byOrg=true",
-            description: "TypeScript versions in use across all repositories in your workspace, " +
+    // TODO cd merge into one call
+    registerCategories(TypeScriptVersion, "Node.js");
+    registerReportDetails(TypeScriptVersion, {
+        name: "TypeScript versions",
+        shortName: "version",
+        unit: "version",
+        url: "fingerprint/typescript-version/typescript-version?byOrg=true",
+        description: "TypeScript versions in use across all repositories in your workspace, " +
             "broken out by version and repositories that use each version.",
-        });
-        registerCategories(NpmDeps, "Node.js");
-        registerReportDetails(NpmDeps, {
-            shortName: "dependency",
-            unit: "version",
-            url: "drift?type=npm-project-deps",
-            description: "Node direct dependencies in use across all repositories in your workspace, " +
+    });
+    registerCategories(NpmDeps, "Node.js");
+    registerReportDetails(NpmDeps, {
+        shortName: "dependency",
+        unit: "version",
+        url: "drift?type=npm-project-deps",
+        description: "Node direct dependencies in use across all repositories in your workspace, " +
             "grouped by Drift Level.",
-        });
-        registerCategories(SpringBootStarter, "Java");
-        registerCategories(JavaBuild, "Java");
-        registerCategories(SpringBootVersion, "Java");
-        registerCategories(DirectMavenDependencies, "Java");
-        registerReportDetails(DirectMavenDependencies, {
-            shortName: "dependency",
-            unit: "version",
-            url: "drift?type=maven-direct-dep",
-            description: "Maven direct dependencies in use across all repositories in your workspace, " +
+    });
+    registerCategories(SpringBootStarter, "Java");
+    registerCategories(JavaBuild, "Java");
+    registerCategories(SpringBootVersion, "Java");
+    registerCategories(DirectMavenDependencies, "Java");
+    registerReportDetails(DirectMavenDependencies, {
+        shortName: "dependency",
+        unit: "version",
+        url: "drift?type=maven-direct-dep",
+        description: "Maven direct dependencies in use across all repositories in your workspace, " +
             "grouped by Drift Level.",
-        });
-        if (isStaging) {
-            registerCategories(LeinDeps, "Java");
-            registerReportDetails(LeinDeps, { url: "drift?type=clojure-project-deps" });
-        }
-        registerCategories(DockerFrom, "Docker");
-        registerReportDetails(DockerFrom, {
-            name: "Docker base images",
-            shortName: "images",
-            unit: "tag",
-            url: "fingerprint/docker-base-image/*?byOrg=true&presence=false&progress=false&otherLabel=false&trim=false",
-            description: "Docker base images in use across all repositories in your workspace, " +
+    });
+    if (isStaging) {
+        registerCategories(LeinDeps, "Java");
+        registerReportDetails(LeinDeps, { url: "drift?type=clojure-project-deps" });
+    }
+    registerCategories(DockerFrom, "Docker");
+    registerReportDetails(DockerFrom, {
+        name: "Docker base images",
+        shortName: "images",
+        unit: "tag",
+        url: "fingerprint/docker-base-image/*?byOrg=true&presence=false&progress=false&otherLabel=false&trim=false",
+        description: "Docker base images in use across all repositories in your workspace, " +
             "broken out by image label and repositories where used.",
-        });
-        registerCategories(DockerPorts, "Docker");
-        registerReportDetails(DockerPorts, {
-            shortName: "ports",
-            unit: "port",
-            url: "fingerprint/docker-ports/*?byOrg=true&presence=false&progress=false&otherLabel=false&trim=false",
-            description: "Ports exposed in Docker configuration in use  across all repositories in your workspace, " +
+    });
+    registerCategories(DockerPorts, "Docker");
+    registerReportDetails(DockerPorts, {
+        shortName: "ports",
+        unit: "port",
+        url: "fingerprint/docker-ports/*?byOrg=true&presence=false&progress=false&otherLabel=false&trim=false",
+        description: "Ports exposed in Docker configuration in use  across all repositories in your workspace, " +
             "broken out by port number and repositories where used.",
-        });
+    });
+    registerReportDetails(LeinDeps, {
+        shortName: "dependency",
+        unit: "version",
+        url: "drift?type=clojure-project-deps",
+        description: "Leiningen direct dependencies in use across all repositories in your workspace, " +
+            "grouped by Drift Level.",
+    });
 
-        if (mode === "online") {
-            const pushImpact = new PushImpact();
+    if (mode === "online") {
+        const pushImpact = new PushImpact();
 
-            sdm.addExtensionPacks(
-                fingerprintSupport({
-                    pushImpactGoal: pushImpact,
-                    aspects: jobAspects,
-                    handlers,
-                }));
+        sdm.addExtensionPacks(
+            fingerprintSupport({
+                pushImpactGoal: pushImpact,
+                aspects: jobAspects,
+                handlers,
+            }));
 
-            return {
-                analyze: {
-                    goals: pushImpact,
-                },
-            };
-        } else {
-            sdm.addEvent(CreateFingerprintJob);
-            sdm.addCommand(CreateFingerprintJobCommand);
-            sdm.addCommand(calculateFingerprintTask(jobAspects, handlers));
-            return {};
-        }
+        return {
+            analyze: {
+                goals: pushImpact,
+            },
+        };
+    } else {
+        sdm.addEvent(CreateFingerprintJob);
+        sdm.addCommand(CreateFingerprintJobCommand);
+        sdm.addCommand(calculateFingerprintTask(jobAspects, handlers));
+        return {};
+    }
 
-    },
+},
     {
         name: "Analysis Software Delivery Machine",
         preProcessors: async cfg => {
@@ -282,7 +289,7 @@ function orgVisualizationEndpoints(dbClientFactory: ClientFactory, httpClientFac
     return {
         routesToSuggestOnStartup:
             [...aboutStaticPages.routesToSuggestOnStartup,
-                ...aboutTheApi.routesToSuggestOnStartup],
+            ...aboutTheApi.routesToSuggestOnStartup],
         customizers: [aboutStaticPages.customizer, aboutTheApi.customizer],
     };
 }
